058f505d2b86845c5dbc8fd3c92ef6cbc239ad8f
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-08-10 17:44:40 +0600

    import json working now

--- import.js
+++ import.js
@@ -10,17 +10,29 @@ document.getElementById("submitButton").addEventListener("click", function () {
 
         if (isValidStructure(importedCollections)) {
           console.log("Imported collections:", importedCollections);
-          Toastify({
-            text: "Success! Collections Imported",
-            duration: 3000,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "right", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-              background: "linear-gradient(to right, #4caf50, #4caf50)",
-            },
-          }).showToast();
+          browser.storage.local.get({ collections: [] }, function (result) {
+            const collections = result.collections;
+            collections.length = 0;
+            collections.push(...importedCollections);
+            console.log(collections);
+            browser.storage.local.set(
+              { collections: collections },
+              function () {
+                Toastify({
+                  text: "Success! Collections Imported",
+                  duration: 3000,
+                  close: true,
+                  gravity: "top", // `top` or `bottom`
+                  position: "right", // `left`, `center` or `right`
+                  stopOnFocus: true, // Prevents dismissing of toast on hover
+                  style: {
+                    background: "linear-gradient(to right, #4caf50, #4caf50)",
+                  },
+                }).showToast();
+              }
+            );
+          });
+
           // Handle the valid data, e.g., save it or process further
         } else {
           Toastify({
